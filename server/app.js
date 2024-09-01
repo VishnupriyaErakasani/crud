e95e6f86ad3811dc03eb2a32330660fb45d2adb7
VishnupriyaErakasani <ekrishnaveni2005@gmail.com>
2024-09-01 11:47:13 +0530

    Your commit message

--- server/app.js
+++ server/app.js
@@ -3,8 +3,9 @@ const mongoose = require('mongoose')
 //const cors=require('cors')
 const studentRouter = require('./routes/students')
 
-const url = 'mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit1?replicaSet=m101';
+//const url = 'mongodb://127.0.0.1:27020,127.0.0.1:27021,127.0.0.1:27022/cbitit1?replicaSet=m101';
 // const url = 'mongodb://127.0.0.1:27017/MGIT'
+const url = 'mongodb+srv://ekrishnaveni2005:9618346705@crud.qwcb2.mongodb.net/?retryWrites=true&w=majority&appName=crud'
 const app = express()
 mongoose.connect(url)
 const con = mongoose.connection
